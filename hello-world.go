9c05367c6e9e433649c4af5590dba99e330722d3
Akinyemi <bisolu.akinyemi@capgemini.com>
2022-04-29 09:54:13 +0100

    new variable

--- hello-world.go
+++ hello-world.go
@@ -1,9 +1,15 @@
 package main
 
 func main() {
-	println("Hello World!")
-	println("Hello academy!")
-	println("Hello academy22!")
-	println("Hello academy24!")
+	// println("Hello World!")
+	// println("Hello academy!")
+	// println("Hello academy22!")
+	// println("Hello academy24!")
+
+		myString := "Hello World!"
+			println(myString)
+			myString = "1234"
+				println(myString)
+				
 
 }
