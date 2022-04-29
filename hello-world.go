576edabe7cc4db0686cf7df7f0f0549b215f35a1
Akinyemi <bisolu.akinyemi@capgemini.com>
2022-04-29 14:51:45 +0100

    Lab 02

--- hello-world.go
+++ hello-world.go
@@ -1,9 +1,21 @@
 package main
 
-func main() {
-	println("Hello World!")
-	println("Hello academy!")
-	println("Hello academy22!")
-	println("Hello academy24!")
+//Lab 01
+ func main() {
+// 	println("Hello World!")
+// 	println("Hello academy!")
+// 	println("Hello academy22!")
+// 	println("Hello academy24!")
+
+//Lab 02
+		var myString string= "Hello World!"
+			println(myString)
+			myString = "1234"
+				println(myString)
+				myString = "Welcome to Go!"
+					println(myString)
+					myString = "5678"
+						println(myString)
+
 
 }
